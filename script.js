767668bb5e3c929262602b71613030c53e82180b
Nico <103645370+NicoLoveCode@users.noreply.github.com>
2023-01-16 17:52:11 +0000

    ws

--- script.js
+++
@@ -1,8 +0,0 @@
-const ScrollBtn = document.querySelector('.scroll-to-top')
-
-console.log(ScrollBtn);
-
-ScrollBtn.addEventListener('click', () => {
-    document.body.top = 0;
-    document.documentElement.top = 0;
-})
